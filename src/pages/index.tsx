4e226268199fe4095cf32ec5d8f9a0a313b83ad5
lemidio <luiz.emidio@chemicalwatch.com>
2023-04-24 09:33:38 -0300

    added code to main section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
 import Banner from '@/components/Banner'
 import Header from '@/components/Header'
+import SmallCard from '@/components/SmallCard'
 import Head from 'next/head'
 
-export default function Home() {
+type Countries = {
+  exploreData: {
+    img: string;
+    location: string;
+    distance: string;
+  }[]
+}
+
+export default function Home(exploreData: Countries) {
+
+  console.log(exploreData.exploreData, 'expl')
+
   return (
     <div className=''>
       <Head>
@@ -10,11 +22,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-
-      {/* Banner */}
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>
+            Explore Nearby
+          </h2>
+          
+          {exploreData.exploreData.map((item, index) => (
+            <SmallCard key={index} img={item.img} distance={item.distance} location={item.location} />
+          ))}
+        </section>
+      </main>
     </div>
   )
+}
+
+export async function  getStaticProps() {
+  const exploreData = await fetch('http://localhost:3001/')
+    .then(
+      (res) => res.json()
+      )
+
+    return { props: {
+      exploreData
+    }}
 }
